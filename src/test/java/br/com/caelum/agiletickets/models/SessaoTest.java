4865f5b5a25fc21b24d8a1153a2b829777d1350a
Heric Branco <hericbranco@gmail.com>
2011-08-30 20:44:49 -0300

    adicionado um metodo before na classe de testes para sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,14 +1,21 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	private Sessao sessao;
+	
+	@Before
+	public void contruct(){
+		sessao = new Sessao();
+	}
 	
 	@Test
 	public void deveVender2IngressosSeHa2Vagas(){
-		Sessao sessao = new Sessao();
+		
 		sessao.setTotalIngressos(2);
 		
 		Assert.assertTrue(sessao.podeReservar(2));
@@ -16,7 +23,7 @@ public class SessaoTest {
 
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(1));
@@ -24,7 +31,7 @@ public class SessaoTest {
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
@@ -32,7 +39,7 @@ public class SessaoTest {
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
+
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
